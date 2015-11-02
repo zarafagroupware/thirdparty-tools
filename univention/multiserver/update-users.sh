8a7b8b308281b424bc7254e7cb58d532f8a4faf4
Felix Bartels <felix@host-consultants.de>
2015-11-02 15:15:28 +0100

    Update update-users.sh

--- univention/multiserver/update-users.sh
+++ univention/multiserver/update-users.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # Update existing (single-server) users inside of the Univention LDAP with their new home server.
-# Replace master in the set line with the name of the desired Zarafa host
+# Please put name of of the desired Zarafa host into the variable "newhost"
 
 eval "$(ucr shell)"
+newhost=master
 
 for i in $( zarafa-admin -l | tail -n +4 | grep -v SYSTEM | awk {'print $1'} ); do
 	univention-directory-manager users/user modify --dn uid=$i,cn=users,$ldap_base \
-	--set z4uUserServer="master"
+	--set z4uUserServer="$newhost"
 done
