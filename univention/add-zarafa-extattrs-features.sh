4825784748fb6e069bbf2d472230c8cd5f64e9a3
Felix Bartels <felix@host-consultants.de>
2016-01-13 11:43:56 +0100

    refine script, add disabled features as well and add a help text to explain the general workflow

--- univention/add-zarafa-extattrs-features.sh
+++ univention/add-zarafa-extattrs-features.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # original source: https://jira.zarafa.com/browse/ZUCS-28
-# Create needed fields in the Univention GUI to manage per user IMAP and Pop3 access
+# This script creates the needed fields in the Univention GUI to manage per user IMAP, Pop3 and Z-Push access
+# These values can be used to override the config option disabled_features in server.cfg.
+# To globally disable a feature add it to disabled_features (by default is "imap pop3"), to globally enable it simply remove it from there.
 #
-# TODO: enable a dropdown or something to enable these features
+# TODO: add a dropdown or something to enable these features
 
 eval "$(ucr shell)"
 
@@ -20,17 +22,33 @@ eval "$(ucr shell)"
 #        #--set syntax=z4uEnabledFeatureSyntax \
 
 univention-directory-manager settings/extended_attribute create "$@" --ignore_exists \
-        --position "cn=zarafa,cn=custom attributes,cn=univention,$ldap_base" \
-        --set name="zarafaEnabledFeature" \
-        --set module=users/user \
-        --set tabName="Zarafa" \
-        --set shortDescription="Enable specific features for User" \
-        --set longDescription="Enable specific features for User" \
-        --set translationShortDescription='"de_DE" "Aktiviere spezifische Features (imap/pop3/mobile) für Benutzer"' \
-        --set translationLongDescription='"de_DE" "Aktiviere spezifische Features (imap/pop3/mobile) für Benutzer"' \
-        --set objectClass=zarafa-user \
+	--position "cn=zarafa,cn=custom attributes,cn=univention,$ldap_base" \
+	--set name="zarafaEnabledFeature" \
+	--set module=users/user \
+	--set tabName="Zarafa" \
+	--set shortDescription="Enable specific features for User" \
+	--set longDescription="Enable specific features for User" \
+	--set translationShortDescription='"de_DE" "Aktiviere spezifische Features (imap/pop3/mobile) für Benutzer"' \
+	--set translationLongDescription='"de_DE" "Aktiviere spezifische Features (imap/pop3/mobile) für Benutzer"' \
+	--set objectClass=zarafa-user \
 	--set syntax=string \
 	--set mayChange=1 \
-        --set ldapMapping=zarafaEnabledFeatures \
-        --set multivalue=1 \
+	--set ldapMapping=zarafaEnabledFeatures \
+	--set multivalue=1 \
 	--set tabPosition=99
+
+univention-directory-manager settings/extended_attribute create "$@" --ignore_exists \
+	--position "cn=zarafa,cn=custom attributes,cn=univention,$ldap_base" \
+	--set name="zarafaDisabledFeature" \
+	--set module=users/user \
+	--set tabName="Zarafa" \
+	--set shortDescription="Disable specific features for User" \
+	--set longDescription="Disable specific features for User" \
+	--set translationShortDescription='"de_DE" "Deaktiviere spezifische Features (imap/pop3/mobile) für Benutzer"' \
+	--set translationLongDescription='"de_DE" "Deativiere spezifische Features (imap/pop3/mobile) für Benutzer"' \
+	--set objectClass=zarafa-user \
+	--set syntax=string \
+	--set mayChange=1 \
+	--set ldapMapping=zarafaDisabledFeatures \
+	--set multivalue=1 \
+	--set tabPosition=100
